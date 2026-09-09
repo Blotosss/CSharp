24b790c394d0d320c4a2fc0c4f6a9298ac212cb8
Blotosss <sasaivladk@gmail.com>
2026-09-09 21:37:10 +0300

    Finish task 1

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Lab01;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Task1.Run();
+    }
+}
---
+++ Lab01/Task1.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab01;
+
+public class Task1
+{
+    public static void Run()
+    {
+        double weight = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        double imt = weight / (height * height);
+
+        Console.WriteLine($"IMT: {imt:F2}");
+    }
+}
