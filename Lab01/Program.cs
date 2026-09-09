039acd14e1ba20ff1b61cbc18dda92ed80c8240f
Blotosss <sasaivladk@gmail.com>
2026-09-09 21:37:38 +0300

    Finish task 2

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Task1.Run();
+        Task2.Run();
     }
 }
---
+++ Lab01/Task2.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Lab01;
+
+public class Task2
+{
+    public static void Run()
+    {
+        double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        int count = int.Parse(Console.ReadLine());
+        int discount = int.Parse(Console.ReadLine());
+
+        double total = price * count * (1 - discount / 100.0);
+
+        Console.WriteLine($"Сума: {total.ToString("F2", CultureInfo.InvariantCulture)} грн");
+    }
+}
