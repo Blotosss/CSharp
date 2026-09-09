0dbeeca99c24cd8133315cb4fe6ed5778eb27e1f
Blotosss <sasaivladk@gmail.com>
2026-09-09 21:39:31 +0300

    Finish task 7

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Task6.Run();
+        Task7.Run();
     }
 }
---
+++ Lab01/Task7.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+
+namespace Lab01;
+
+public class Task7
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        decimal[] costs = new decimal[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            costs[i] = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        decimal sum = 0;
+        decimal min = costs[0];
+        decimal max = costs[0];
+
+        foreach (var cost in costs)
+        {
+            sum += cost;
+            if (cost < min) min = cost;
+            if (cost > max) max = cost;
+        }
+
+        decimal average = sum / n;
+
+        int aboveAverageCount = 0;
+        foreach (var cost in costs)
+        {
+            if (cost > average)
+            {
+                aboveAverageCount++;
+            }
+        }
+
+        string firstExpensive = "немає";
+        int index = 0;
+        while (index < n)
+        {
+            if (costs[index] > 1000m)
+            {
+                firstExpensive = $"#{index + 1} – {costs[index].ToString("F2", CultureInfo.InvariantCulture)} грн";
+                break;
+            }
+            index++;
+        }
+
+        Console.WriteLine("=== Звіт по прийомах ===");
+        Console.WriteLine($"Кількість:      {n}");
+        Console.WriteLine($"Загальна сума:  {sum.ToString("F2", CultureInfo.InvariantCulture)} грн");
+        Console.WriteLine($"Середня:        {average.ToString("F2", CultureInfo.InvariantCulture)} грн");
+        Console.WriteLine($"Мін / Макс:     {min.ToString("F2", CultureInfo.InvariantCulture)} / {max.ToString("F2", CultureInfo.InvariantCulture)} грн");
+        Console.WriteLine($"Вище середнього: {aboveAverageCount} з {n}");
+        Console.WriteLine($"Перший > 1000:  {firstExpensive}");
+    }
+}
