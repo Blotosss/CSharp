4e318bc36cd006b335961e3c2396b3fe9ae3583f
Blotosss <sasaivladk@gmail.com>
2026-09-15 20:44:23 +0300

    Finish task 2 (Lab-02 branch)

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -4,6 +4,6 @@ class Proram
 {
     public static void Main()
     {
-        Task1.Run();
+        Task2.Run();
     }
 }
---
+++ Lab02/Task2.cs
@@ -0,0 +1,38 @@
+﻿namespace Lab02;
+
+public class Task2
+{
+    public static void Run()
+    {
+        int number = int.Parse(Console.ReadLine()); 
+        int[] price = new int[number];
+        
+        for (int i = 0; i < number; i++)
+        {
+            price[i] = int.Parse(Console.ReadLine());
+        }
+        
+        string beforeSort = string.Join(" ", price);
+        
+        for (int i = 0; i < number - 1; i++)
+        {
+            for (int j = 0; j < number - i - 1; j++)
+            {
+                if (price[j] > price[j + 1])
+                {
+                    (price[j], price[j + 1]) = (price[j + 1], price[j]);
+                }
+            }
+        }
+        
+        string afterSort = string.Join(" ", price);
+        
+        int min = price[0];
+        int max = price[number - 1];
+        
+        Console.WriteLine($"Черга (до):     {beforeSort}");
+        Console.WriteLine($"Черга (після):  {afterSort}");
+        Console.WriteLine($"Найдешевший:    {min} грн");
+        Console.WriteLine($"Найдорожчий:    {max} грн");
+    }
+}
