9db95b4d83c72dacf146d4414a51e48f1ca29ca8
Blotosss <sasaivladk@gmail.com>
2026-09-25 20:18:13 +0300

    Lab03 Task02

--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("=== Task 01: Patients ===");
         Patient p1 = new Patient("Іван", "Петренко", new DateTime(1983, 5, 15), "A+", "0501234567");
         Patient p2 = new Patient("Олена", "Коваль", new DateTime(1991, 11, 20), "B-", "0672345678");
         Patient p3 = new Patient("Максим", "Бойко", new DateTime(2008, 3, 10), "0+", "0933456789");
@@ -17,5 +18,20 @@ class Program
         Console.WriteLine(p3);
         Console.WriteLine(p4);
         Console.WriteLine(p5);
+
+        Console.WriteLine("\n=== Task 02: Doctors ===");
+        Doctor d1 = new Doctor("Олег", "Сидоренко", "Кардіологія", "LIC-001", "0441234567");
+        d1.WorkStartHour = 8;
+        d1.WorkEndHour = 16;
+
+        Doctor d2 = new Doctor("Наталія", "Мороз", "Неврологія", "LIC-002", "0442345678");
+        d2.WorkStartHour = 9;
+        d2.WorkEndHour = 18;
+
+        Doctor d3 = new Doctor("Андрій", "Власенко", "Педіатрія", "LIC-003", "0443456789");
+
+        Console.WriteLine(d1);
+        Console.WriteLine(d2);
+        Console.WriteLine(d3);
     }
 }
